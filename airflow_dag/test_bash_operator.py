da21fc529276662ad1203bff351ee5b5adf82df5
GPX99 <86992620+GPX99@users.noreply.github.com>
2024-07-05 05:08:49 +0800

    feat: add airflow dags

---
+++ airflow_dag/test_bash_operator.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+DOC_MD = """
+This is to test max_active_tasks
+"""
+
+DAG_ID = Path(__file__).stem
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.utcnow(),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    schedule=None,
+    doc_md=DOC_MD,
+) as dag:
+    BashOperator(
+        task_id="bash_task",
+        cmds=[
+            "echo",
+            "this is bash operator",
+        ]
+    )
---
+++ airflow_dag/test_k8s_operator.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
+
+
+DOC_MD = """
+This is to test max_active_tasks
+"""
+
+DAG_ID = Path(__file__).stem
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.utcnow(),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    default_args=default_args,
+    schedule=None,
+    doc_md=DOC_MD,
+) as dag:
+    KubernetesPodOperator(
+        task_id="k8s_task",
+        image="alpine:latest",
+        cmds=[
+            "echo",
+            "this is k8s operator",
+        ],
+    )
